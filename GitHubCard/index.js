6135eb409585499c9ec019fe7a8789c048a14d57
Mike Nguyen <mpvnguyen1996@gmail.com>
2019-09-05 16:19:07 -0400

    intial commit + finished project

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -2,6 +2,79 @@
            (replacing the palceholder with your Github name):
            https://api.github.com/users/<your name>
 */
+  axios
+  .get("https://api.github.com/users/mpvn30")
+  .then(response => {
+    console.log(response);
+    const data = response.data;
+    const create = createCard(data);
+    entry.appendChild(create);
+  })
+  .catch(error => {
+    console.log("The data was not returned", error);
+  });
+
+  axios
+  .get("https://api.github.com/users/tetondan")
+  .then(response => {
+    console.log(response);
+    const data = response.data;
+    const create = createCard(data);
+    entry.appendChild(create);
+  })
+  .catch(error => {
+    console.log("The data was not returned", error);
+  });
+
+  axios
+  .get("https://api.github.com/users/dustinmyers")
+  .then(response => {
+    console.log(response);
+    const data = response.data;
+    const create = createCard(data);
+    entry.appendChild(create);
+  })
+  .catch(error => {
+    console.log("The data was not returned", error);
+  });
+
+  axios
+  .get("https://api.github.com/users/justsml")
+  .then(response => {
+    console.log(response);
+    const data = response.data;
+    const create = createCard(data);
+    entry.appendChild(create);
+  })
+  .catch(error => {
+    console.log("The data was not returned", error);
+  });
+
+  axios
+  .get("https://api.github.com/users/luishrd")
+  .then(response => {
+    console.log(response);
+    const data = response.data;
+    const create = createCard(data);
+    entry.appendChild(create);
+  })
+  .catch(error => {
+    console.log("The data was not returned", error);
+  });
+
+  axios
+  .get("https://api.github.com/users/bigknell")
+  .then(response => {
+    console.log(response);
+    const data = response.data;
+    const create = createCard(data);
+    entry.appendChild(create);
+  })
+  .catch(error => {
+    console.log("The data was not returned", error);
+  });
+
+
 
 /* Step 2: Inspect and study the data coming back, this is YOUR 
    github info! You will need to understand the structure of this 
@@ -13,6 +86,7 @@
 /* Step 4: Pass the data received from Github into your function, 
            create a new component and add it to the DOM as a child of .cards
 */
+const entry = document.querySelector(".cards");
 
 /* Step 5: Now that you have your own card getting added to the DOM, either 
           follow this link in your browser https://api.github.com/users/<Your github name>/followers 
@@ -45,6 +119,50 @@ const followersArray = [];
 </div>
 
 */
+function createCard(obj){
+  const
+    newCard = document.createElement("div"),
+    newImage = document.createElement("img"),
+    cardInfo = document.createElement("div"),
+    name = document.createElement("h3"),
+    userName = document.createElement("p"),
+    userLocation = document.createElement("p"),
+    userProfile = document.createElement("p"),
+    gitAddress = document.createElement("a"),
+    userFollowers = document.createElement("p"),
+    userFollowing = document.createElement("p"),
+    userBio = document.createElement("p");
+
+    newImage.src = obj.avatar_url;
+    name.textContent = obj.name;
+    userName.textContent = obj.login;
+    userLocation.textContent = obj.location;
+    userProfile.textContent = `Profile: `;
+    gitAddress.href = obj.html_url;
+    gitAddress.textContent = obj.html_url;
+    userFollowers.textContent = `Followers: ${obj.followers}`;
+    userFollowing.textContent = `Following: ${obj.following}`;
+    userBio.textContent = `Bio: ${obj.bio}`;
+
+    newCard.classList.add("card");
+    cardInfo.classList.add("card-info");
+    name.classList.add("name");
+    userName.classList.add("username");
+
+    newCard.appendChild(newImage);
+    newCard.appendChild(cardInfo);
+    cardInfo.appendChild(name);
+    cardInfo.appendChild(userName);
+    cardInfo.appendChild(userLocation);
+    cardInfo.appendChild(userProfile);
+    userProfile.appendChild(gitAddress);
+    cardInfo.appendChild(userFollowers);
+    cardInfo.appendChild(userFollowing);
+    cardInfo.appendChild(userBio);
+
+    return newCard;
+
+}
 
 /* List of LS Instructors Github username's: 
   tetondan
